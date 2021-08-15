c3c2d848f64d46c18ed0e7c0144670c6b7940d3b
andrezaserra <acbserra@gmail.com>
2021-08-15 02:07:13 -0300

    initial commit - config pt 3

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'rack-attack'
 
 #authentication
 gem 'devise', '~> 4.4'
+gem 'simple_token_authentication', '~> 1.0'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,14 @@
 module Api::V1
- 
  class ApiController < ApplicationController
  
+  acts_as_token_authentication_handler_for User
+  before_action :require_authentication!
    # Métodos globais
  
+  private
+    def require_authentication!
+      twrow(:warden, scope:, :user) unless current_user.presence   
+    end
+
  end
- 
 end
